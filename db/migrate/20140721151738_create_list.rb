c812b3fdb19b2656dfb4f8ffc980cce7844b253a
Ryan Holm <ryan.j.holm@gmail.com>
2014-08-03 22:31:56 -0600

    updated create list.

--- db/migrate/20140721151738_create_list.rb
+++ db/migrate/20140721151738_create_list.rb
@@ -2,10 +2,12 @@ class CreateList < ActiveRecord::Migration
   def change
     create_table :lists do |t|
       t.string  :name
-      t.boolean :public,       default: true
+      t.boolean :public, default: true
       t.text    :description
+      t.belongs_to :user
 
       t.timestamps
     end
   end
 end
+
